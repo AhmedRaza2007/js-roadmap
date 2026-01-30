9a68cc77096ad969d0108da8b18b9e5eb3b4002f
Ahmed Raza <a.razakz2007@gmail.com>
2026-01-30 11:15:34 +0500

    added object file main

---
+++ objects.js
@@ -0,0 +1,179 @@
+const fromName = {
+    first: "Ahmed",
+    last: "Raza",
+    age: 20,
+    city: "Karachi",
+    favouriteColor: "Black"
+}
+console.log(fromName);
+const myHobbies = {
+    hobby1: "Reading Books",
+    hobby2: "Traveling",
+    hobby3: "Coding",
+    hobby4: "Photography",
+    hobby5: "Gardening",
+    hobby6: "Gaming"
+}
+console.log(myHobbies);
+const myFavoriteMeals = {
+    breakfast: "Paratha and Omelette",
+    lunch: "Biryani",
+    dinner: "white chicken karahi with naan",
+    snacks: "Samosas, Pakoras, and Fruit Chaat"
+}
+console.log(myFavoriteMeals);
+const myDreams = {
+    dream1: "Become a Successful Developer",
+    dream2: "Travel the World",
+    dream3: "Start a Tech Company",
+    dream4: "Visit Madinah",
+    dream5: "Build a Mosque",
+    dream6: "Learn Multiple Languages",
+    dream7: "Islamic Knowledge",
+    dream8: "Quran Memorization",
+    dream9: "Perform Umrah",
+    dream10: "Perform Hajj"
+}
+console.log(myDreams);
+
+const myDreamphone = {
+    brand: "Apple",
+    model: "iPhone 17 Pro Max",
+    storage: "1TB",
+    color: "Silver",
+    camera: "48MP",
+    battery: "5000mAh"
+}
+console.log(myDreamphone);
+
+const fromAge = {
+    first: "Sara",
+    last: "Khan",
+    age: 25,
+    city: "Lahore",
+    favouriteColor: "Blue"
+}
+console.log(fromAge);
+const myLife = {
+  personalInfo: {
+    firstName: "Ahmed",
+    lastName: "Raza",
+    city: "Karachi",
+    age: 19
+  },
+
+  education: [
+    {
+      level: "Matric",
+      institute: "Government High School Tharushah",
+      year: 2025,
+      status: "Completed"
+    },
+    {
+      level: "Intermediate",
+      institute: "Government College",
+      year: 2026,
+      status: "In Progress"
+    }
+  ],
+
+  skills: ["HTML", "CSS", "JavaScript", "GitHub"],
+
+  goals: [
+    { title1: "Become a Software Developer" },
+    { title2: "Start Own Business" },
+    { title3: "Help Parents" },
+    { title4: "Perform Umrah" }
+  ],
+
+  dailyRoutine: {
+    wakeUp: "Fajr",
+    study: "3 Hours Coding",
+    work: "Practice Projects",
+    sleep: "12 AM"
+  }
+};
+console.log(myLife);
+const classFriends = [
+    {
+        first: "Ali",
+        last: "Ahmed",
+        age: 30,
+        city: "Islamabad",
+        favouriteColor: "Green"
+    },
+    {
+        first: "Zainab",
+        last: "Khalid",
+        age: 28,
+        city: "Peshawar",
+        favouriteColor: "Purple"
+    },
+    {
+        first: "Hassan",
+        last: "Javed",
+        age: 22,
+        city: "Quetta",
+        favouriteColor: "Red"
+    },
+    {
+        first: "Ayesha",
+        last: "Malik",
+        age: 27,
+        city: "Faisalabad",
+        favouriteColor: "Yellow"
+    },
+]
+console.log(classFriends);
+
+const familyMembers = [
+    {
+        first: "Muhammad",
+        last: "Furqan",
+        age: 45,
+        city: "Karachi",
+        area: "BTk"
+    },
+    {
+        first: "Muhammad",
+        last: "Farhan",
+        age: 40,
+        city: "Karachi",
+        area: "Naya Nazimabad"
+    },
+    {
+        first: "Muhammad",
+        last: "Shaheem",
+        age: 45,
+        city: "Karachi",
+        area: "Manghopir"
+    },
+]
+console.log(familyMembers);
+const cars = [
+  {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    color: "White",
+    price: 4200000,
+    isAutomatic: true
+  },
+  {
+    brand: "Honda",
+    model: "Civic",
+    year: 2019,
+    color: "Black",
+    price: 5200000,
+    isAutomatic: true
+  },
+  {
+    brand: "Suzuki",
+    model: "Alto",
+    year: 2022,
+    color: "Silver",
+    price: 2300000,
+    isAutomatic: false
+  }
+];
+console.log(cars);
